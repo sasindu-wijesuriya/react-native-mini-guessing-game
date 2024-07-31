84b7ac863cef07990b9e518d687a8a3e2bdadd75
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-07-31 15:51:46 +0530

    Add colors.js file with primary color constants

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+  primaryPlum: "#72063c",
+  primaryPlumDark: "#4e0329",
+  primaryYellow: "#ddb52f",
+  primaryButtonBackground: "#f7287b",
+  primaryButtonText: "#eee",
+};
+
+export default Colors;
