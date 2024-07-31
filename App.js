9847b5962939c06255ba4c8595e6e6c43c48456f
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-07-31 15:52:55 +0530

    Refactor App.js and add background image

--- App.js
+++ App.js
@@ -1,8 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import Colors from "./constants/colors";
+
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  const [userNumber, setUserNumber] = useState();
+
+  function startGameHandler(selectedNumber) {
+    setUserNumber(selectedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={startGameHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen pickedNumber={userNumber} />;
+  }
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[Colors.primaryPlumDark, Colors.primaryYellow]}
+    >
+      <ImageBackground
+        source={require("./assets/images/bg_image.jpg")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={{ opacity: 0.15 }}
+      >
+        <SafeAreaView style={styles.container}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
